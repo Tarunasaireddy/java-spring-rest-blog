6369a060826cd65d805a206f974eeebafcc73f6b
Tarunasaireddy <49843582+Tarunasaireddy@users.noreply.github.com>
2023-01-16 17:22:23 +0900

    Update DatabaseLoader.java

--- src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
+++ src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
@@ -1,4 +1,21 @@
- public List<Post> randomPosts = new ArrayList<>();
+package com.pluralsight.blog.data;
+import com.pluralsight.blog.model.Author;	
+import com.pluralsight.blog.model.Post;	
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;	
+import org.springframework.boot.ApplicationRunner;	
+import org.springframework.stereotype.Component;	
+import java.util.ArrayList;	
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;	
+   @Component
+public class DatabaseLoader implements ApplicationRunner {
+    private final String[] templates = {	 
+     "Smart Home %s", "Mobile %s - For When You're On he Go", "The %s - Your New Favorite Accessory"};
+ private final String[] gadgets = {
+  "Earbuds", "Speakers", "Tripod", "Instant Pot", "Coffee Cup", "Keyboard", "Sunglasses"};
+    public List<Post> randomPosts = new ArrayList<>();
     public List<Author> authors = new ArrayList<>();
 
     private final PostRepository postRepository;
